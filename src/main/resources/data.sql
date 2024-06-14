9ce95f5005fd3b07455db72a85b11b2079225004
AstridSilaghi <astrid.silaghi@gmail.com>
2024-06-14 23:18:27 +0300

    Insert data in ordes and order_product table via sql script initializer

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,17 @@ INSERT INTO products ( product_name, price, description, availability)
 VALUES ('Product3', 724.7, 'Description for Product3', 'N' );
 INSERT INTO products (product_name, price, description, availability) 
 VALUES ('Product4', 510, 'Description for Product4', 'Y' );
+
+INSERT INTO orders (order_number, order_date) 
+VALUES ('Order1000', CURRENT_DATE);
+INSERT INTO orders (order_number, order_date) 
+VALUES ('Order1001', CURRENT_DATE);
+INSERT INTO orders (order_number, order_date) 
+VALUES ('Order1002', CURRENT_DATE);
+
+INSERT INTO order_product (order_id, product_id) 
+VALUES (1, 2);
+INSERT INTO order_product (order_id, product_id) 
+VALUES (1, 3);
+INSERT INTO order_product (order_id, product_id) 
+VALUES (2, 3);
